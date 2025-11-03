d571c6f6476b8e93d616658b0aa5a265687d9854
MisterPetz <104466360+MisterPetz@users.noreply.github.com>
2025-11-03 16:39:40 -0600

    Final implementation

--- pilot/apps/johannes/bootcamp_app.py
+++ pilot/apps/johannes/bootcamp_app.py
@@ -4,7 +4,7 @@ import random
 from pyrosetta import *
 from pyrosetta.rosetta.numeric import random
 from pyrosetta.rosetta.protocols import moves
-
+from pyrosetta.rosetta import core
 init(extra_options="-ignore_unrecognized_res")
 
 parser = argparse.ArgumentParser()
@@ -25,6 +25,14 @@ the_observer.pymol().apply(mypose)
 N_residues = mypose.total_residue()
 mc = moves.MonteCarlo(mypose, sxfn, 1.0)
 
+# Set up MoveMap for backbone and sidechain movement
+movemap = core.kinematics.MoveMap()
+movemap.set_bb(True)
+movemap.set_chi(True)
+# Minimizer setup
+min_opts = core.optimization.MinimizerOptions("lbfgs_armijo_atol", 0.01, True)
+minimizer = core.optimization.AtomTreeMinimizer()
+
 for i in range(2):
     # Perturb phi and psi
     rand_residue = random.uniform()
@@ -35,4 +43,15 @@ for i in range(2):
     orig_psi = mypose.psi(rand_residue)
     mypose.set_phi(rand_residue, orig_phi + phi_pert)
     mypose.set_psi(rand_residue, orig_psi + psi_pert)
+    #add packing and minimization calls to 
+    tf = core.pack.task.TaskFactory()
+    task = tf.create_task_and_apply_taskoperations(mypose)
+    task.restrict_to_repacking()
+    core.pack.pack_rotamers(mypose, sxfn, task)
+    
+
+    minimizer.run(mypose, movemap, sxfn, min_opts)
     mc.boltzmann(mypose)
+    
+print(f"Score: {sxfn.score(mypose)}")
+mypose.dump_pdb("Output.pdb")
